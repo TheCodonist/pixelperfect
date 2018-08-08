1d60b97a3540511fa8dff14e95a8e6d28d1f12d4
Hamayun Aziz <hamayunaziz@codonist.com>
2018-08-08 20:24:11 +0500

    initial

---
+++ pp/pixelperfect.js
@@ -0,0 +1,20 @@
+/**
+ * Created by AitchKay on 16-Feb-17.
+ */
+
+(function () {
+    
+    
+
+}).call(this);
+
+jQuery.pixelperfect = function() {
+    var code = '<div class="pixelPerfect" style="position: absolute; top: 0; right: 0; left: 0; opacity: 0.3; z-index: 999999999">' +
+        '<img class="" src="userlist.png" alt="match it" style="width: 100%">' +
+        '<a href="#" id="pp-btn" style="position: fixed; top: 0; right: 0; padding: 4px; background-color: #000; font-size: 10px; line-height: 1.1; color: #fff">PP</a>' +
+        '</div>';
+    
+    console.log('called')
+};
+
+$.pixelperfect();
